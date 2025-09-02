5ef749070dbf09fb5c3528960593f6ebc2c705de
Joshh-D <dayapera.joshuabscs2024@gmail.com>
2025-09-03 00:09:44 +0800

    Front End, Numbers Printing, Clear Screen

--- Calculator/Form1.Designer.vb
+++ Calculator/Form1.Designer.vb
@@ -22,10 +22,322 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.Del = New System.Windows.Forms.Button()
+        Me.Divide = New System.Windows.Forms.Button()
+        Me.Multiply = New System.Windows.Forms.Button()
+        Me.Minus = New System.Windows.Forms.Button()
+        Me.Plus = New System.Windows.Forms.Button()
+        Me.Equals = New System.Windows.Forms.Button()
+        Me.Point = New System.Windows.Forms.Button()
+        Me.B0 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Clear
+        '
+        Me.Clear.BackColor = System.Drawing.Color.DarkGray
+        Me.Clear.Font = New System.Drawing.Font("Segoe UI Semibold", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Clear.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Clear.Location = New System.Drawing.Point(11, 170)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(208, 100)
+        Me.Clear.TabIndex = 58
+        Me.Clear.Text = "C"
+        Me.Clear.UseVisualStyleBackColor = False
+        '
+        'Del
+        '
+        Me.Del.BackColor = System.Drawing.Color.DarkGray
+        Me.Del.Font = New System.Drawing.Font("Segoe UI Semibold", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Del.Location = New System.Drawing.Point(227, 170)
+        Me.Del.Name = "Del"
+        Me.Del.Size = New System.Drawing.Size(100, 100)
+        Me.Del.TabIndex = 57
+        Me.Del.Text = "Del"
+        Me.Del.UseVisualStyleBackColor = False
+        '
+        'Divide
+        '
+        Me.Divide.BackColor = System.Drawing.Color.Gold
+        Me.Divide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Divide.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Divide.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Divide.Location = New System.Drawing.Point(339, 170)
+        Me.Divide.Name = "Divide"
+        Me.Divide.Size = New System.Drawing.Size(100, 100)
+        Me.Divide.TabIndex = 56
+        Me.Divide.Text = "/"
+        Me.Divide.UseVisualStyleBackColor = False
+        '
+        'Multiply
+        '
+        Me.Multiply.BackColor = System.Drawing.Color.Gold
+        Me.Multiply.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Multiply.Location = New System.Drawing.Point(337, 276)
+        Me.Multiply.Name = "Multiply"
+        Me.Multiply.Size = New System.Drawing.Size(100, 100)
+        Me.Multiply.TabIndex = 55
+        Me.Multiply.Text = "x"
+        Me.Multiply.UseVisualStyleBackColor = False
+        '
+        'Minus
+        '
+        Me.Minus.BackColor = System.Drawing.Color.Gold
+        Me.Minus.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Minus.Location = New System.Drawing.Point(337, 382)
+        Me.Minus.Name = "Minus"
+        Me.Minus.Size = New System.Drawing.Size(100, 100)
+        Me.Minus.TabIndex = 54
+        Me.Minus.Text = "-"
+        Me.Minus.UseVisualStyleBackColor = False
+        '
+        'Plus
+        '
+        Me.Plus.BackColor = System.Drawing.Color.Gold
+        Me.Plus.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Plus.Location = New System.Drawing.Point(337, 488)
+        Me.Plus.Name = "Plus"
+        Me.Plus.Size = New System.Drawing.Size(100, 100)
+        Me.Plus.TabIndex = 53
+        Me.Plus.Text = "+"
+        Me.Plus.UseVisualStyleBackColor = False
+        '
+        'Equals
+        '
+        Me.Equals.BackColor = System.Drawing.Color.Gold
+        Me.Equals.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Equals.Location = New System.Drawing.Point(227, 596)
+        Me.Equals.Name = "Equals"
+        Me.Equals.Size = New System.Drawing.Size(210, 100)
+        Me.Equals.TabIndex = 52
+        Me.Equals.Text = "="
+        Me.Equals.UseVisualStyleBackColor = False
+        '
+        'Point
+        '
+        Me.Point.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.Point.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Point.ForeColor = System.Drawing.SystemColors.Window
+        Me.Point.Location = New System.Drawing.Point(119, 596)
+        Me.Point.Name = "Point"
+        Me.Point.Size = New System.Drawing.Size(100, 100)
+        Me.Point.TabIndex = 51
+        Me.Point.Text = "."
+        Me.Point.UseVisualStyleBackColor = False
+        '
+        'B0
+        '
+        Me.B0.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B0.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B0.ForeColor = System.Drawing.SystemColors.Window
+        Me.B0.Location = New System.Drawing.Point(11, 596)
+        Me.B0.Name = "B0"
+        Me.B0.Size = New System.Drawing.Size(100, 100)
+        Me.B0.TabIndex = 50
+        Me.B0.Text = "0"
+        Me.B0.UseVisualStyleBackColor = False
+        '
+        'B9
+        '
+        Me.B9.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B9.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B9.ForeColor = System.Drawing.SystemColors.Window
+        Me.B9.Location = New System.Drawing.Point(227, 276)
+        Me.B9.Name = "B9"
+        Me.B9.Size = New System.Drawing.Size(100, 100)
+        Me.B9.TabIndex = 49
+        Me.B9.Text = "9"
+        Me.B9.UseVisualStyleBackColor = False
+        '
+        'B8
+        '
+        Me.B8.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B8.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B8.ForeColor = System.Drawing.SystemColors.Window
+        Me.B8.Location = New System.Drawing.Point(119, 276)
+        Me.B8.Name = "B8"
+        Me.B8.Size = New System.Drawing.Size(100, 100)
+        Me.B8.TabIndex = 48
+        Me.B8.Text = "8"
+        Me.B8.UseVisualStyleBackColor = False
+        '
+        'B7
+        '
+        Me.B7.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B7.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B7.ForeColor = System.Drawing.Color.White
+        Me.B7.Location = New System.Drawing.Point(11, 276)
+        Me.B7.Name = "B7"
+        Me.B7.Size = New System.Drawing.Size(100, 100)
+        Me.B7.TabIndex = 47
+        Me.B7.Text = "7"
+        Me.B7.UseVisualStyleBackColor = False
+        '
+        'B6
+        '
+        Me.B6.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B6.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B6.ForeColor = System.Drawing.SystemColors.Window
+        Me.B6.Location = New System.Drawing.Point(227, 382)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(100, 100)
+        Me.B6.TabIndex = 46
+        Me.B6.Text = "6"
+        Me.B6.UseVisualStyleBackColor = False
+        '
+        'B5
+        '
+        Me.B5.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B5.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B5.ForeColor = System.Drawing.SystemColors.Window
+        Me.B5.Location = New System.Drawing.Point(119, 382)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(100, 100)
+        Me.B5.TabIndex = 45
+        Me.B5.Text = "5"
+        Me.B5.UseVisualStyleBackColor = False
+        '
+        'B4
+        '
+        Me.B4.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B4.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B4.ForeColor = System.Drawing.SystemColors.Window
+        Me.B4.Location = New System.Drawing.Point(11, 382)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(100, 100)
+        Me.B4.TabIndex = 44
+        Me.B4.Text = "4"
+        Me.B4.UseVisualStyleBackColor = False
+        '
+        'B3
+        '
+        Me.B3.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B3.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B3.ForeColor = System.Drawing.SystemColors.Window
+        Me.B3.Location = New System.Drawing.Point(227, 488)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(100, 100)
+        Me.B3.TabIndex = 43
+        Me.B3.Text = "3"
+        Me.B3.UseVisualStyleBackColor = False
+        '
+        'B2
+        '
+        Me.B2.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B2.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B2.ForeColor = System.Drawing.SystemColors.Window
+        Me.B2.Location = New System.Drawing.Point(119, 488)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(100, 100)
+        Me.B2.TabIndex = 42
+        Me.B2.Text = "2"
+        Me.B2.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.WindowText
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(13, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(426, 128)
+        Me.Label1.TabIndex = 41
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(119, 488)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(0, 0)
+        Me.Button15.TabIndex = 40
+        Me.Button15.Text = "Button15"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'B1
+        '
+        Me.B1.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.B1.Font = New System.Drawing.Font("Segoe UI Semibold", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B1.ForeColor = System.Drawing.SystemColors.Window
+        Me.B1.Location = New System.Drawing.Point(11, 488)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(100, 100)
+        Me.B1.TabIndex = 39
+        Me.B1.Text = "1"
+        Me.B1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(119, 170)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(0, 0)
+        Me.Button2.TabIndex = 38
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackgroundImage = Global.Calculator.My.Resources.Resources.Screenshot_2025_09_02_234521
+        Me.ClientSize = New System.Drawing.Size(452, 703)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.Del)
+        Me.Controls.Add(Me.Divide)
+        Me.Controls.Add(Me.Multiply)
+        Me.Controls.Add(Me.Minus)
+        Me.Controls.Add(Me.Plus)
+        Me.Controls.Add(Me.Equals)
+        Me.Controls.Add(Me.Point)
+        Me.Controls.Add(Me.B0)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Clear As Button
+    Friend WithEvents Del As Button
+    Friend WithEvents Divide As Button
+    Friend WithEvents Multiply As Button
+    Friend WithEvents Minus As Button
+    Friend WithEvents Plus As Button
+    Friend WithEvents Equals As Button
+    Friend WithEvents Point As Button
+    Friend WithEvents B0 As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents B7 As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button15 As Button
+    Friend WithEvents B1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- Calculator/Form1.vb
+++ Calculator/Form1.vb
@@ -1,3 +1,26 @@
 ﻿Public Class Form1
+    Private Sub Equals_Click(sender As Object, e As EventArgs) Handles Equals.Click
+        Label1.Text = "HELLO WORLD!"
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        Label1.Text = ""
+    End Sub
+
+    Private Sub Number_Click(sender As Object, e As EventArgs) _
+Handles B0.Click, B1.Click, B2.Click, B3.Click, B4.Click, B5.Click,
+        B6.Click, B7.Click, B8.Click, B9.Click, Point.Click, Plus.Click,
+        Minus.Click, Multiply.Click, Divide.Click
+
+
+        Dim btn As Button = CType(sender, Button)
+
+        If Label1.Text = "0" Then
+            Label1.Text = btn.Text
+        Else
+            Label1.Text &= btn.Text
+        End If
+    End Sub
+
 
 End Class
