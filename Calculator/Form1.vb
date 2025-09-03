161167d9973776cbe9c3b4bc1e43e1fabf614b0f
ashbeyds <delossantos.ashbeybscs2024@gmail.com>
2025-09-03 21:45:36 +0800

    add button function

--- Calculator/Form1.vb
+++ Calculator/Form1.vb
@@ -1,6 +1,19 @@
 ﻿Public Class Form1
+    Private firstNum As Double = 0
+    Private currentNumber As String = ""
+    Private isOperatorPressed As Boolean = False
     Private Sub Equals_Click(sender As Object, e As EventArgs) Handles Equals.Click
-        Display.Text = "HELLO WORLD!"
+        Dim secondNum As Double
+        If Double.TryParse(Display.Text, secondNum) Then
+            Select Case currentNumber
+                Case "+"
+                    Display.Text = (firstNum + secondNum).ToString()
+                Case "-"
+                    Display.Text = (firstNum = secondNum).ToString()
+                Case "x"
+                    Display.Text = (firstNum * secondNum).ToString()
+            End Select
+        End If
     End Sub
 
     Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
@@ -12,13 +25,20 @@ Handles B0.Click, B1.Click, B2.Click, B3.Click, B4.Click, B5.Click,
         B6.Click, B7.Click, B8.Click, B9.Click, Point.Click, Plus.Click,
         Minus.Click, Multiply.Click, Divide.Click
 
-
         Dim btn As Button = CType(sender, Button)
 
-        If Display.Text = "0" Then
-            Display.Text = btn.Text
+        If btn.Text = "+" OrElse btn.Text = "-" OrElse btn.Text = "x" OrElse btn.Text = "/" Then
+            If Double.TryParse(Display.Text, firstNum) Then
+                currentNumber = btn.Text
+                isOperatorPressed = True
+            End If
         Else
-            Display.Text &= btn.Text
+            If Display.Text = "0" OrElse isOperatorPressed Then
+                Display.Text = btn.Text
+                isOperatorPressed = False
+            Else
+                Display.Text &= btn.Text
+            End If
         End If
     End Sub
 
